b5dbe6d7c195b5639f1fe551e07b8951b5cae168
Xavi Aparicio <xavier.aparicio@softonic.com>
2022-05-31 14:12:26 +0200

    Added tests

---
+++ tests/PreProcessors/EnsureModelExistsTest.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace Softonic\RestApiNestedResources\PreProcessors;
+
+use Illuminate\Database\Query\Builder;
+use Mockery;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
+
+class EnsureModelExistsTest extends TestCase
+{
+    private Builder $builder;
+
+    private $model;
+
+    private EnsureModelExists $ensureModelExists;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->builder = Mockery::mock(Builder::class);
+        $this->model   = Mockery::mock('alias:PlatformVersion');
+
+        $this->ensureModelExists = new EnsureModelExists();
+    }
+
+    protected function tearDown(): void
+    {
+        Mockery::close();
+
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     */
+    public function whenModelExistsItShouldDoNothing()
+    {
+        $this->builder->shouldReceive('count')
+            ->once()
+            ->andReturn(1);
+
+        $this->model->shouldReceive('where')
+            ->once()
+            ->with([
+                'id_platform' => 'windows',
+                'id_version'  => 'test',
+            ])
+            ->andReturn($this->builder);
+
+        $parameters = [
+            'id_program'         => 'f0f1ae26-e44a-460a-9f59-f53c83ec4372',
+            'id_platform'        => 'windows',
+            'id_version'         => '1.0',
+            'id_platformversion' => 'test',
+        ];
+
+        $result = $this->ensureModelExists->process(
+            'PlatformVersion',
+            ['id_platform', 'id_version=id_platformversion'],
+            $parameters
+        );
+
+        self::assertNull($result);
+    }
+
+    /**
+     * @test
+     */
+    public function whenModelDoesNotExistItShouldThrowAnException()
+    {
+        $this->builder->shouldReceive('count')
+            ->once()
+            ->andReturn(0);
+
+        $this->model->shouldReceive('where')
+            ->once()
+            ->with([
+                'id_platform' => 'windows',
+                'id_version'  => 'test',
+            ])
+            ->andReturn($this->builder);
+
+        $parameters = [
+            'id_program'         => 'f0f1ae26-e44a-460a-9f59-f53c83ec4372',
+            'id_platform'        => 'windows',
+            'id_version'         => '1.0',
+            'id_platformversion' => 'test',
+        ];
+
+        $parametersToCheck = [
+            'id_platform' => 'windows',
+            'id_version'  => 'test',
+        ];
+        $this->expectException(ConflictHttpException::class);
+        $this->expectExceptionMessage(
+            'PlatformVersion resource does not exist for ' . json_encode($parametersToCheck)
+        );
+
+        $this->ensureModelExists->process(
+            'PlatformVersion',
+            ['id_platform', 'id_version=id_platformversion'],
+            $parameters
+        );
+    }
+
+    public function fieldsDoesNotExistProvider(): array
+    {
+        return [
+            'without any parameter'    => [
+                [
+                    'id_program'  => 'f0f1ae26-e44a-460a-9f59-f53c83ec4372',
+                    'id_platform' => 'windows',
+                ],
+            ],
+            'without some parameter#2' => [
+                [
+                    'id_program'         => 'f0f1ae26-e44a-460a-9f59-f53c83ec4372',
+                    'id_platformversion' => 'test',
+                ],
+            ],
+        ];
+    }
+
+
+    /**
+     * @test
+     *
+     * @dataProvider fieldsDoesNotExistProvider
+     */
+    public function whenFieldsDoesNotExistItShouldDoNothing(array $parameters)
+    {
+        $result = $this->ensureModelExists->process(
+            'PlatformVersion',
+            ['id_platform', 'id_version=id_platformversion'],
+            $parameters
+        );
+
+        self::assertNull($result);
+    }
+}
